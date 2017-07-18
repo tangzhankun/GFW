97f34e1252b79d3994ae334f8bbd88c2063769ab
root <root@localhost.(none)>
2017-07-18 15:52:58 +0000

    init GFW

---
+++ GFW/start_vpnserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run -d --privileged --net=host -v /root/pptp/chap-secrets:/etc/ppp/chap-secrets mobtitude/vpn-pptp
+docker run -d -p 500:500/udp -p 4500:4500/udp -p 1701:1701/tcp -e PSK=woaiyixi -e USERNAME=tangzhankun -e PASSWORD=woaiyixi siomiz/softethervpn
+docker run -d -p 1984:1984 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 1984 -k woaiyixi -m aes-256-cfb
