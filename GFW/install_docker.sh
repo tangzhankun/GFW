262c23f233c95c08a64e7bcf76df1de17e6df59b
tangzhankun <tangzhankun@gmail.com>
2017-10-28 05:06:48 +0000

    update install docker

--- GFW/install_docker.sh
+++ GFW/install_docker.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-git clone https://github.com/tangzhankun/GFW.git
 cp pptp -r /root/
-cd /root
-apt-get install openssh-server
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce
-cp GFW/GFW/start_vpnserver.sh ./
-./start_vpnserver.sh
+apt-get install -y docker-ce
